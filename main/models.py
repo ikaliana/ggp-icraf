060adcc6e355d6d67050fbbe60409997b15eb7fd
ikaliana <ikaliana@yahoo.com>
2016-10-11 17:11:29 +0700

    working with csv

--- main/models.py
+++ main/models.py
@@ -1,5 +1,49 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from adaptor.fields import *
+from adaptor.model import CsvModel
 
 # Create your models here.
+
+class History(models.Model):
+	ID_T1 = CharField()
+	ID_T2 = CharField()
+	ID_Z = CharField()
+	Area = IntegerField()
+	Period = CharField()
+	AreaName = CharField()
+	PlanningUnit = CharField()
+	PeatType = CharField()
+	LandcoverName_T1 = CharField()
+	Carbon_T1 = FloatField()
+	Peat_T1 = FloatField()
+	LandcoverName_T2 = CharField()
+	Carbon_T2 = FloatField()
+	Peat_T2 = FloatField()
+	LandProfit_T1 = FloatField()
+	LandProfit_T2 = FloatField()
+
+class HistoryCsv(CsvModel):
+	ID_T1 = CharField(match = "ID_LC_T1")
+	ID_T2 = CharField(match = "ID_LC_T2")
+	ID_Z = CharField(match = "ID_Z")
+	Area = IntegerField(match = "COUNT_AREA")
+	Period = CharField(match = "PERIOD")
+	AreaName = CharField(match = "ADMIN")
+	PlanningUnit = CharField(match = "PLAN")
+	PeatType = CharField(match = "PEAT")
+	LandcoverName_T1 = CharField(match = "LC_T1")
+	Carbon_T1 = FloatField(match = "C_T1")
+	Peat_T1 = FloatField(match = "P_T1")
+	LandcoverName_T2 = CharField(match = "LC_T2")
+	Carbon_T2 = FloatField(match = "C_T2")
+	Peat_T2 = FloatField(match = "P_T2")
+	LandProfit_T1 = FloatField(match = "PROF_T1")
+	LandProfit_T2 = FloatField(match = "PROF_T2")
+
+	class Meta:
+		#delimiter = ','
+		dbModel = History
+		#has_header = True
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from django.templatetags.static import static
 
 # Create your views here.
 def index(request):
 	return render(request, 'index.html', {})
+
+def testcsv(request):
+	csvPath = 'data/Historical_analysis.csv'
+	#csvPath = 'css/materialize.css'
+	staticPath = "D:\Indra\GGP\WebApp\main\static\data\Historical_analysis.csv" #static(csvPath)
+	#resolvePath = resolve(csvPath)
+	test = HistoryCsv.import_from_filename(staticPath)
+	#test = HistoryCsv.import_data(data = open(staticPath))
+
+	output = "<p># %s</p>" % HistoryCsv.has_class_delimiter() #staticPath
+	return HttpResponse(output)
+
+	#return render(request, "blank.html", {})
